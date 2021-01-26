75ec0db6c3ddf73cf6403855a3de9a3c4bf0dbe7
SakeenaTraif <s.traif30@gmail.com>
2021-01-26 10:06:56 +0300

    SearchBar Task

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,11 +1,10 @@
-import React from "react";
 // Styling
 import { SearchBarStyled } from "../styles";
 
 const SearchBar = (props) => {
   return (
     <SearchBarStyled
-      onChange={(event) => console.log(event.target.value)}
+      onChange={(event) => props.setQuery(event.target.value.toLowerCase())}
       placeholder="Search for a cookie name"
     />
   );
